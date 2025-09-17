c4b0f5ce21c3d8db56caef795034a473bc0bc1c1
inhoang0909 <yenhoanglv0205@gmail.com>
2025-09-17 15:27:33 +0700

    fix/send data log

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,10 @@
-# Dockerfile
+FROM node:20
 
-# Sử dụng Node.js base image
-FROM node:18-alpine
-
-# Tạo thư mục làm việc trong container
 WORKDIR /app
 
-# Sao chép package.json và package-lock.json trước để cache dependency layer
 COPY package*.json ./
+RUN npm install --production
 
-# Cài đặt dependencies
-RUN npm install
-
-# Sao chép toàn bộ mã nguồn vào container
 COPY . .
 
-# Mở cổng ứng dụng (ví dụ 3000)
-EXPOSE 3000
-
-# Lệnh khởi động ứng dụng
-CMD ["npm", "start"]
+CMD ["node", "index.js"]
--- middleware/requestLoggerMiddleware.js
+++ middleware/requestLoggerMiddleware.js
@@ -2,22 +2,24 @@ import logger from "../services/loggerService.js";
 export default function requestLoggerMiddleware(req, res, next) {
   res.on("finish", async () => {
     try {
-      let ip = req.ip || req.connection.remoteAddress || "";
-      if (ip.startsWith("::ffff:")) {
-        ip = ip.substring(7);
+      if (req.originalUrl === "/translate" || req.originalUrl.startsWith("/translate?")) {
+        let ip = req.ip || req.connection.remoteAddress || "";
+        if (ip.startsWith("::ffff:")) {
+          ip = ip.substring(7);
+        }
+
+        const logData = {
+          service: "translate-api-service",
+          endpoint: req.originalUrl,
+          method: req.method,
+          status: res.statusCode,
+          ip,
+          date: new Date().toISOString().slice(0, 10),
+          time: new Date(),
+        };
+
+        await logger.send(logData);
       }
-
-      const logData = {
-        service: "translate-api-service",
-        endpoint: req.originalUrl,
-        method: req.method,
-        status: res.statusCode,
-        ip,
-        date: new Date().toISOString().slice(0, 10),
-        time: new Date(),
-      };
-
-      await logger.send(logData);
     } catch (error) {
       console.error("Logging error:", error);
     }
